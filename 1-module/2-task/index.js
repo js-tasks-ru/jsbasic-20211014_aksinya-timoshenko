ce6006024a27bc881f1639f449eefd2dc22d776f
Aksinya <aksinya2016@gmail.com>
2021-10-25 15:14:04 +0300

    1-module

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  return (name != null && name != "" && name.length == name.replace(/\s+/g,'').length && name.length >= 4);
 }
 
 function sayHello() {
