8f7ea172395cd0bf81b3fe821a984ef2ebedd032
Aksinya <aksinya2016@gmail.com>
2021-10-27 17:24:52 +0300

    2module

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
 let calculator = {
-  // ваш код
+  a: 0,
+  b: 0,
+  read(_a,_b){
+    this.a = _a;
+    this.b = _b;
+  },
+  sum(){
+    return this.a + this.b;
+  },
+  mul(){
+  return this.a * this.b;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
